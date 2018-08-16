49c73f099a4fadc0e3de4b6c3a3790fa7a831327
suvendukumar <42138535+suvendukumar@users.noreply.github.com>
2018-08-16 15:08:34 +0000

    Add files via upload

---
+++ subha.cpp
@@ -0,0 +1,25 @@
+//library
+#include<iostream>
+using namespace std;
+//writing main function
+int main(){
+//declaring variables
+int a=8,b=4;
+int c,d,e,f,g;
+//addition of two numbers
+c=a+b;
+//substraction of two numbers
+d=a-b;
+//multiplication of two numbers
+e=a*b;
+//division of two numbers
+f=a/b;
+//remainder of two numbers
+g=a%b;
+cout<<"sum of two numbers="<< c << endl;
+cout<<"substraction of two numbers="<< d << endl;
+cout<<"multiplication of two numbers="<< e << endl;
+cout<<"division of two numbers="<< f << endl;
+cout<<"remainder of two numbers="<< g << endl;
+return 0;
+}
---
+++ suvendu.cpp
@@ -0,0 +1,21 @@
+//library
+#include<iostream>
+using namespace std;
+
+
+
+
+
+int main(){
+cout<< " hello "<< endl;
+     // Declaring variables of all types
+int value;
+int a,b; // declaring variables together also possible 
+       double doublevar =1.2 ; // defining the variables.
+ bool boolvar = true;
+// displaying the sizes of data types.
+cout<< "size of a = "<< sizeof(a) << endl;
+cout << " size of doublevar = "<<sizeof(doublevar)<< endl;
+cout <<"size of boolvar = " <<sizeof(boolvar)<<endl;
+cout <<"size of char = "<< sizeof(char)<<endl;
+} 
