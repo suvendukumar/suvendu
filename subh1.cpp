9d30f76e3664fafdca2997a79ae4bd7290975fd5
suvendukumar <42138535+suvendukumar@users.noreply.github.com>
2018-08-22 01:26:32 -0400

    Add files via upload

---
+++ subh1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+// main function
+int main()
+{
+float cel,far;
+cout<<"input temp in celcius and convert into farenheit "<<"\n";
+cout<<"input temp in celcius="<<"\n";
+cin>>cel;
+far=1.8*cel +32;
+cout<<"temperature in farenheit scale="<<far<<endl;
+return 0;
+}
+
