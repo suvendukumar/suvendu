bcb5fbf0b57c13b653b899b45ec62fb15993c594
suvendukumar <42138535+suvendukumar@users.noreply.github.com>
2018-08-22 01:47:05 -0400

    Add files via upload

---
+++ subh3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+// main function
+int main()
+{
+float base,height,area;
+cout<<"find the area of triangle"<<"\n";
+cout<<"input base of triangle="<<"\n";
+cin>>base;
+cout<<"input height of triangle="<<"\n";
+cin>>height;
+area=0.5*base*height;
+cout<<"area of triangle="<<area<<endl;
+return 0;
+}
+
+
