60fe9e50c1d13109677dc2750b10907dcc5e5434
suvendukumar <42138535+suvendukumar@users.noreply.github.com>
2018-08-22 02:00:54 -0400

    Add files via upload

---
+++ subh4.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+// main function
+int main()
+{
+float area,side;
+//calculation of area of equilateral triangle
+cout<<"calculate area of equilateral triangle"<<"\n";
+cout<<"input side of equilateral triangle="<<"\n";
+//inputting side of equilateral triangle
+cin>>side;
+//formulae for calculating area 
+area=0.4325*side*side;
+//area of equilateral triangle
+cout<<"area of equilateral triangle="<<area<<endl;
+//returning value 0
+return 0;
+}
